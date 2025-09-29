ff3abc170650128ecdd4a5dee3efc4ecc6e3238b
valerii.korniienko <vakor.dev@gmail.com>
2025-09-29 11:47:27 +0300

    :hammer: CQRS abstractions

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommand : IRequest<Unit>;
+
+public interface ICommand<out TResponse> : IRequest<TResponse>
+    where TResponse : notnull;
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,14 @@
-﻿namespace Catalog.API.Products.CreateProduct;
+﻿using BuildingBlocks.CQRS;
 
-public class CreateProductHandler {
-    
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) 
+    : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand,CreateProductResult> {
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken) {
+        throw new NotImplementedException();
+    }
 }
