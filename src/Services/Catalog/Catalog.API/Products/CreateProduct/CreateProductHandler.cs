e941c9e8e98dedbfa777b10d5d012f1b05e824e2
valerii.korniienko <vakor.dev@gmail.com>
2025-10-02 23:51:26 +0300

    :hammer: Dummy Validator

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,12 +1,29 @@
-﻿namespace Catalog.API.Products.CreateProduct;
+﻿using ValidationException = System.ComponentModel.DataAnnotations.ValidationException;
 
-public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) 
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand,CreateProductResult> {
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand> {
+    public CreateProductCommandValidator() {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+    }
+}
+
+internal class CreateProductCommandHandler(IDocumentSession session, IValidator<CreateProductCommand> validator)
+    : ICommandHandler<CreateProductCommand, CreateProductResult> {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken) {
+        var result = await validator.ValidateAsync(command, cancellationToken);
+        var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+        if (errors.Any())
+            throw new ValidationException(errors.FirstOrDefault());
+
         var product = new Product {
             Name = command.Name,
             Category = command.Category,
@@ -14,10 +31,10 @@ internal class CreateProductCommandHandler(IDocumentSession session) : ICommandH
             ImageFile = command.ImageFile,
             Price = command.Price,
         };
-        
+
         session.Store(product);
         await session.SaveChangesAsync(cancellationToken);
-        
+
         return new CreateProductResult(product.Id);
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -5,6 +5,7 @@ builder.Services.AddCarter();
 builder.Services.AddMediatR(config => {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 builder.Services.AddMarten(opts => {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 })
