bc5ee0071d9133f0642a25721c90f78a56b6243d
valerii.korniienko <vakor.dev@gmail.com>
2025-10-06 14:01:15 +0300

    :hammer: Global exception handler

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -4,5 +4,7 @@ global using MediatR;
 global using Marten;
 global using FluentValidation;
 global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Behaviours;
+global using BuildingBlocks.Exceptions.Handler;
 global using Catalog.API.Models;
 global using Catalog.API.Exceptions;
