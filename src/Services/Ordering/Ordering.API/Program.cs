5addedfbd6dedd708929d9fd4624a99a95f4e99d
Valerii Korniienko <myksv200@gmail.com>
2025-10-21 20:11:41 +0300

    :hammer: Di finished

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,8 +1,18 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to container
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+
 var app = builder.Build();
 
+app.UseApiServices();
 // Configure Http request pipeline
 
 app.Run();
