8f4eebf2da9e1c3869183d59924d93ac1e34b5ed
Valerii Korniienko <myksv200@gmail.com>
2025-11-07 22:09:33 +0200

    :hammer: Basic configurations

--- src/Services/Ordering/Ordering.Domain/Events/OrderCreatedEvent.cs
+++ src/Services/Ordering/Ordering.Domain/Events/OrderCreatedEvent.cs
@@ -1,4 +1,3 @@
 ﻿namespace Ordering.Domain.Events;
 
 public record OrderCreatedEvent(Order Order) : IDomainEvent;
-public record OrderUpdatedEvent(Order Order) : IDomainEvent;
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/IApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+
+namespace Ordering.Infrastructure.Data;
+
+public interface IApplicationDbContext
+{
+    DbSet<Customer> Customers { get; }
+    DbSet<Product> Products { get; }
+    DbSet<Order> Orders { get; }
+    DbSet<OrderItem> OrderItems { get; }
+}
